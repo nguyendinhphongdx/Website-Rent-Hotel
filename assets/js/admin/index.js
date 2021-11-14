6dd1b47bf240183ca2b181cf56948f59f2bad2cc
nguyendinhphongdx <phongnguyendx@gmail.com>
2021-11-14 22:45:12 +0700

    update admin

--- assets/js/admin/index.js
+++ assets/js/admin/index.js
@@ -6,9 +6,32 @@ const toBase64 = file => new Promise((resolve, reject) => {
     reader.onerror = error => reject(error);
 });
 const handleOnChangeInputImage = async (e,des="")=>{
-    const base = await toBase64(e.files[0]);
-    const destination =  document.getElementById(des);
-    if(destination){
-        destination.setAttribute("src", base);
+    const images = document.getElementsByClassName("image")[0];
+    const files = e.files;
+    console.log(files);
+    for (let index = 0; index < files.length; index++) {
+        const element = files[index];
+        const base = await toBase64(element);
+         const newImage = document.createElement("img");
+         newImage.setAttribute("src", base);
+         images.appendChild(newImage);
     }
+}
+const onLoad=()=>{
+    ListenEvent();
+}
+const ListenEvent = () =>{
+    const buttonsScroll = document.getElementsByClassName("scroll-card-info");
+    for (let index = 0; index < buttonsScroll.length; index++) {
+        buttonsScroll[index].addEventListener("click",()=>ScrollToCardInfor());
+        
+    }
+}
+const ScrollToCardInfor = () => {
+    const cardInfor = document.getElementById("card-info");
+    window.scrollTo({
+        top:cardInfor.getBoundingClientRect().height,
+        left:0,
+        behavior:'smooth'
+    });
 }
